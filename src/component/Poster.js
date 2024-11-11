028387894aeb5969b6896f0d4ce278291ac6565e
jeje <akhmdjzl17@gmail.com>
2024-11-11 08:38:38 +0700

    Yawkkkkkksk

--- src/component/Poster.js
+++ src/component/Poster.js
@@ -4,32 +4,9 @@ import meet_mini from '../img/meet_mini.png'
 import { useEffect, useRef } from 'react';
 
 function Poster() {
-    const imgRef = useRef(null);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('slide-in');
-                    observer.unobserve(entry.target); // Berhenti mengamati setelah animasi dijalankan
-                }
-            });
-        }, { threshold: 0.1 }); // Memulai animasi ketika 10% dari elemen terlihat
-
-        if (imgRef.current) {
-            observer.observe(imgRef.current);
-        }
-
-        return () => {
-            if (imgRef.current) {
-                observer.unobserve(imgRef.current);
-            }
-        };
-    }, []);
-
     return (
         <div className="wadah_poster">
-            <img ref={imgRef} src={poster} className='poster_img' />
+            <img src={poster} className='poster_img' />
             <div className='wadah_poster_about'>
                 <Row>
                     <Col xl={8} lg={8} md={8} sm={6} xs={6}>
